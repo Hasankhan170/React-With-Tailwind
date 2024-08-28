05d35c6f729e430de54b2245dc27901adb27dcc7
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-28 09:31:52 -0700

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,53 @@
 function Navbar(){
     return (
         <>
-        <header>
+        <header style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            backgroundColor: 'lightgray',
+            padding: '10px',
+            flexWrap: 'wrap'
+        }}>
             <div className="first">
-                <h1>React Components</h1>
+                <h2>React Components</h2>
             </div>
-            <div className="second">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+            <div>
+                <ul style={{
+                    listStyleType: 'none',
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    alignItems: 'center',
+                    gap: '10px',
+                    fontSize: '18px',
+                    textDecoration: 'none',
+                    fontWeight: 'bold',
+                    flexWrap : 'wrap'
+                }}>
+                    <li>
+                        <a style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                    }} href="#">Home</a>
+                    </li>
+                    <li><a style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                    }} href="#">About</a>
+                    </li>
+
+                    <li>
+                        <a style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                    }} href="#">Services</a>
+                    </li>
+                    <li>
+                        <a style={{
+                        textDecoration: 'none',
+                        color: 'black',
+                    }} href="#">Contact</a>
+                    </li>
                 </ul>
             </div>
         </header>
