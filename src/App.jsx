d9a2e3710c42962541d16a5422435f65f9e3ba5f
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-28 08:53:56 -0700

    simple style

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+    return (
+        <h1 style={{
+            color : "white",
+            backgroundColor: "black",
+            textAlign: "center",
+            padding: "10px"
+        }}>hello world!</h1>
+    )
+}
+
+export default App;
