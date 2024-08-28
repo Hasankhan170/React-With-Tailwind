309f60499af87461c8db7aee1d61520c734119f6
Hasankhan170 <hasankhanali0316@gmail.com>
2024-08-28 09:18:11 -0700

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "../Components/Navbar"; 
+import Navbar from './components/Navbar';
 
 function App(){
     return (
         <>
-        <Navbar />
+        <Navbar/>
         </>
+
     )
 
 }
